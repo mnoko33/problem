137f2a932d25f3cd7033a402f4e1383c3bc237c7
sangtaek lee <srs99125@naver.com>
2019-03-25 19:56:10 +0900

    0325

--- [백준16954]움직이는_미로_탈출.py
+++ [백준16954]움직이는_미로_탈출.py
@@ -39,3 +39,37 @@ while True:
         break
 
 print(ans)
+
+#
+# from copy import deepcopy
+# game = []
+# for _ in range(8):
+#    s = input()
+#    game.append(s)
+#
+# copygame = []
+# diff = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1), (0, 0)]
+# visited = [[False for _in range(8)] for_ in range(8)]
+# cnt = 0
+# def wuk(x,y,game):
+#    global cnt
+#    if x == 0 and y == 7:
+#        cnt += 1
+#        return
+#    visited[x][y] = True
+#    for (dx, dy) in diff:
+#        copygame = deepcopy(game)
+#        tx = dx+x
+#        ty = dy+y
+#        if 0<= tx< 8 and 0<= ty < 8 and visited[tx][ty] == False and copygame[tx][ty] == '.' and copygame[x][y] != '#':
+#            copygame.insert(0,'........')
+#            del copygame[-1]
+#            wuk(tx,ty,copygame)
+#            visited[tx][ty] = False
+#
+#
+# wuk(7,0,game)
+# if cnt >0:
+#    print('1')
+# elif cnt == 0:
+#    print('0')
